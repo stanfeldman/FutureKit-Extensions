9b4d8ca248f6f5baf0d8dde95efeb57fe3dc48de
Stanislav Feldman <stanislavfeldman@gmail.com>
2015-07-15 22:49:17 +0800

    added several methods for Parse integration

--- FutureKit+Parse.swift
+++ FutureKit+Parse.swift
@@ -33,3 +33,71 @@ extension PFQuery {
         return promise.future
     }
 }
+
+extension PFObject {
+    func saveInBackgroundInFuture() -> Future<Bool> {
+        return self.saveInBackgroundInFuture(.Primary)
+    }
+    
+    func saveInBackgroundInFuture(executor:Executor) -> Future<Bool> {
+        let promise = Promise<Bool>()
+        executor.execute {
+            self.saveInBackgroundWithBlock { (success, error) in
+                if error == nil && success {
+                    promise.completeWithSuccess(success)
+                }
+                else if error != nil {
+                    promise.completeWithFail(error!)
+                }
+                else {
+                    promise.completeWithFail("Some error")
+                }
+            }
+        }
+        return promise.future
+    }
+    
+    func fetchIfNeededInFuture() -> Future<PFObject> {
+        return self.fetchIfNeededInFuture(.Primary)
+    }
+    
+    func fetchIfNeededInFuture(executor:Executor) -> Future<PFObject> {
+        let promise = Promise<PFObject>()
+        executor.execute {
+            self.fetchIfNeededInBackgroundWithBlock({ (object, error) -> Void in
+                if error == nil && object != nil {
+                    promise.completeWithSuccess(object!)
+                }
+                else if error != nil {
+                    promise.completeWithFail(error!)
+                }
+                else {
+                    promise.completeWithFail("Some error")
+                }
+            })
+        }
+        return promise.future
+    }
+    
+    class func saveAllInBackgroundInFuture(objects: [AnyObject]) -> Future<Bool> {
+        return PFObject.saveAllInBackgroundInFuture(.Primary, objects:objects)
+    }
+    
+    class func saveAllInBackgroundInFuture(executor:Executor, objects: [AnyObject]) -> Future<Bool> {
+        let promise = Promise<Bool>()
+        executor.execute {
+            PFObject.saveAllInBackground(objects) { (success, error) in
+                if error == nil && success {
+                    promise.completeWithSuccess(success)
+                }
+                else if error != nil {
+                    promise.completeWithFail(error!)
+                }
+                else {
+                    promise.completeWithFail("Some error")
+                }
+            }
+        }
+        return promise.future
+    }
+}
